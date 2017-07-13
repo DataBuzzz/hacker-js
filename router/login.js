d0ccea3df7d0231c5c50457ebde326e25e711c67
Roushan <roushan.jha0709@gmail.com>
2017-07-13 15:08:57 +0530

    Created routes

---
+++ router/login.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var router = express.Router();
+var passport= require('passport');
+var LocalStrategy= require('passport-local').Strategy;
+var User=require('../model/user')
+
+router.get('/', function(req, res, next) {
+
+	res.render('login');
+});
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+   
+   User.getUserByUsername(username, function(err, user) {
+   	  
+   	  if (err) throw err;
+   	  if (!user) {
+   	  	return done(null, false, {message: 'Unknow User'});
+
+   	  }
+
+   	  User.comparePassword(password, user.password,function (err, isMatch) {
+   	     if (err) throw err;
+   	     if (isMatch) {
+   	     	return done(null, user);
+
+   	     }else {
+            return done(null, false, {message:'Invalid password'});
+   	     }
+   	  });
+   });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+router.post('/login',
+  passport.authenticate('local', {succesRedirect:"/", failureRedirect:"/login"}),
+  function(req, res) {
+
+  	res.redirect('/');
+            
+  });
+
+
+module.exports = router;
